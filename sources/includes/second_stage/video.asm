f5b7dc94ba538cf432c6462136a78693dbd2188d
mzaieda <53624383+mzaieda@users.noreply.github.com>
2020-05-03 13:37:59 +0200

    Update video.asm

--- sources/includes/second_stage/video.asm
+++ sources/includes/second_stage/video.asm
@@ -3,10 +3,9 @@
 %define VIDEO_BUFFER_EFFECTIVE_ADDRESS          0xB8000
 %define VIDEO_SIZE      0X0FA0    ; 25*80*2
     video_cls_16:
-            pusha                                   ; Save all general purpose registers on the stack
-
-                  ; This function need to be written by you.
-
+            pusha                                   ; Save all general purpose registers on the stac
+            ; This function need to be written by you.
+            
             popa                                ; Restore all general purpose registers from the stack
             ret
 
